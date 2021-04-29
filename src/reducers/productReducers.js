7f12b4ae8985ae33b576f2cc8ebee844095c5730
Shiseri <sergei.kozinecs@gmail.com>
2021-04-29 21:58:47 +0300

    products and product reducer

--- src/reducers/productReducers.js
+++ src/reducers/productReducers.js
@@ -1,11 +1,18 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstants'
+import {
+    PRODUCT_LIST_FAIL,
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS
+} from '../constants/productConstants'
 
 export const productListReducer = (state = { products: [] }, action) => {
     switch (action.type) {
         case PRODUCT_LIST_FAIL:
             return {
-                loading: true,
-                products: [],
+                loading: false,
+                error: action.payload,
             }
         case PRODUCT_LIST_SUCCESS:
             return {
@@ -14,8 +21,30 @@ export const productListReducer = (state = { products: [] }, action) => {
             }
         case PRODUCT_LIST_REQUEST:
             return {
+                loading: true,
+                products: [],
+            }
+        default:
+            return state
+    }
+}
+
+export const productDetailsReducer = (state = { product: { reviews: [] } }, action) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_FAIL:
+            return {
                 loading: false,
-                error: action.payload
+                error: action.payload,
+            }
+        case PRODUCT_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                product: action.payload
+            }
+        case PRODUCT_DETAILS_REQUEST:
+            return {
+                loading: true,
+                ...state,
             }
         default:
             return state
